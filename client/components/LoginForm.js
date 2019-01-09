e41d230d99cfb68222cc5586a22a1fdff6728e24
Scott Fox <scottfox87@gmail.com>
2019-01-09 17:03:11 -0600

    enhance logic for login

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+import mutation from '../mutations/Login';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class LoginForm extends Component {
+
+    onSubmit({ email, password }) {
+        this.props.mutate({
+            variables: { email, password },
+            refetchQueries: [{ query }]
+        });
+    }
+
     render() {
         return (
             <div>
-                Login Form
+                <h3>Login</h3>
+                <AuthForm onSubmit={this.onSubmit.bind(this)} />
             </div>
         );
     }
 }
 
-export default LoginForm;
+export default graphql(mutation)(LoginForm);
